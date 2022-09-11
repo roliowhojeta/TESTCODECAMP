9736d4ab6c378d9c37a8dd7b1854407bc3fae767
roliowhojeta <yvonnejeta@gmail.com>
2022-09-11 14:49:38 +0100

    added Java project task 14

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!"
+                +"\n The models are called software development system."
+                +"\n Every other model has faded away majorly."
+                +"\n Because of their disadvantages."
+                +"\n A phase begins if the previous phase is completed."
+                +"\n Phases do not overlap."
+                +"\n The outcome of one phase affects the other."
+                +"\n The two most used models are agile and waterfall."
+                +"\n Agile has twelve manifestos.");
+
+
+    }
+
+
+}
